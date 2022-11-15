937135efde8f8b6826cbb708602281d324c4cbd8
Daniel91239 <mahdi.1001.500@gmail.com>
2022-11-15 15:21:28 -0800

    ایکون های غذا اضافه شده و دیزاین انها تمام شد.همچنین بخش بندی شدن غذا ها

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:restaruant_danial/constants/colors.dart';
+import 'package:restaruant_danial/models/restaurant.dart';
+import 'package:restaruant_danial/widgets/FoodListView.dart';
 import 'package:restaruant_danial/widgets/custom_app_bar.dart';
+import 'package:restaruant_danial/widgets/food_list.dart';
 import 'package:restaruant_danial/widgets/restaurant_info.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,7 +12,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  var selected = 0;
+  final pageController=PageController();
+  final restaurant=Restaurant.generateRestaurant();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +25,23 @@ class _HomePageState extends State<HomePage> {
             CustomAppBar(
                 Icons.arrow_back_ios_new_outlined, Icons.search_outlined),
             RestaurantInfo(),
+            FoodList(selected, (int index) {
+              setState(() {
+                selected = index;
+              });
+              pageController.jumpToPage(index);
+            }, restaurant),
+            Expanded(
+              child: FoodListView(
+                selected,(int index){
+                setState(() {
+                  selected= index;
+                });
+              },
+                pageController,
+                restaurant,
+              ),
+            ),
           ],
         )
     );
